9ff91fd87ecdb6fa6049d0f0d40ef88e529e589f
Simone Scarduzio <simone@readonlyrest.com>
2021-11-16 14:18:01 +0100

    make uploads work well

--- main.py
+++ main.py
@@ -13,13 +13,19 @@ from requests.structures import CaseInsensitiveDict
 import json
 import re
 from datetime import datetime
+import imagesize
+import os
+import magic
+import urllib.parse
 
 ################# CONFIGURATION ################################################################################
-JIRA_RSS_EXPORT_FILE = "./done_tasks.xml"
-SPACES_URL = "beshu.jetbrains.space"
+JIRA_RSS_EXPORT_FILE = "./allIssues16.nov.xml"
+SPACES_URL = "https://beshu.jetbrains.space"  # https://YOUR_ORG.jetbrains.space
 SPACES_PROJECT_ID = "23tkZF01SLKP"  # pick this up from API playground prepopulated values (see below URL)
-API_AUTH_TOKEN = "eyJhbGciOiJSUzUxMiJ9.eyJzdWIiOiIyOFBjZkcwZm00S2siLCJhdWQiOiJjaXJjbGV0LXdlYi11aSIsIm9yZ0RvbWFpbiI6ImJlc2h1Iiwic2NvcGUiOiIqKiIsIm5hbWUiOiJzc2NhcmR1emlvIiwiaXNzIjoiaHR0cHM6XC9cL2Jlc2h1LmpldGJyYWlucy5zcGFjZSIsInByaW5jaXBhbF90eXBlIjoiVVNFUiIsImV4cCI6MTYzNjM1OTU0MCwiaWF0IjoxNjM2MzU4OTQwLCJzaWQiOiI0T2xrV0E0Rlp0VnUifQ.OYu7PZSYv_2UiOy4tlZlgeLPdOdBYy-fVKjaQrrhWaUBPMofN_sJZn6jlwDU-igWYBpJ05h37XZ_rvnGHAWpcG9N-eGebMptC6XegZZlc7nF1SSZtuhbqjRTCRyMY865Jc1GjGWNiz0RYjlsz94-Xr1c0oJcQnuDFpOeA4ZS2cE"  # take a temporary token from example code in https://beshu.jetbrains.space/httpApiPlayground?resource=projects_xxx_planning_issues&endpoint=http_post_import
+API_AUTH_TOKEN = os.getenv(
+    "TOKEN")  # You get this from https://YOUR_ORG.jetbrains.space/extensions/installedApplications/<NEW_APP_YOU_MUST_CREATE>/permanent-tokens
 
+JIRA_ISSUE_ID_PREFIX = "RORDEV-"
 JIRA_STATUS_TO_SPACE_STATUS_ID = {
     # grep "<status"  all_tasks.xml | grep -vi done | grep -v "&lt"| sed -e 's/<[^>]*>//g' | sort | uniq
     "To Do": "3mLmz93RuAl9",  # Open
@@ -38,6 +44,15 @@ JIRA_LABEL_TO_SPACE_TAG_ID = {
 
 
 ##############################################################################################################
+
+def mkHeaders(accept="application/json", contentType="application/json"):
+    headers = CaseInsensitiveDict()
+    if accept:
+        headers["Accept"] = accept
+    if contentType:
+        headers["Content-Type"] = contentType
+    headers["Authorization"] = "Bearer " + API_AUTH_TOKEN
+    return headers
 
 
 def jiraStatus2spaceStatusId(jiraStatus):
@@ -107,11 +122,7 @@ def parseJiraDump(doc, jiraId2usernameMap):
 
 
 def insert(issue):
-    url = "https://" + SPACES_URL + "/api/http/projects/id:" + SPACES_PROJECT_ID + "/planning/issues"
-    headers = CaseInsensitiveDict()
-    headers["Accept"] = "application/json"
-    headers["Content-Type"] = "application/json"
-    headers["Authorization"] = "Bearer " + API_AUTH_TOKEN
+    url = SPACES_URL + "/api/http/projects/id:" + SPACES_PROJECT_ID + "/planning/issues"
 
     descAndComments = issue["description"]
 
@@ -134,7 +145,7 @@ def insert(issue):
     })
     print("> Saving issue to Jetbrains Space " + issue["title"])
     pprint(data)
-    resp = requests.post(url, headers=headers, data=data)
+    resp = requests.post(url, headers=mkHeaders(), data=data)
     print(resp.status_code)
 
 
@@ -152,10 +163,140 @@ def scanForJiraUsers(doc):
     return id2usernameMap
 
 
+def scanIssuesIdMap():
+    def getPage(offset):
+        url = SPACES_URL + "/api/http/projects/id:" + SPACES_PROJECT_ID + \
+              "/planning/issues?sorting=TITLE&descending=false&$skip=" + str(offset)
+        resp = requests.get(url, headers=mkHeaders())
+        print(resp.status_code)
+        return json.loads(resp.content)
+
+    offset = 0
+    allIssues = []
+    while True:
+        print("Fetching next " + str(offset) + " issues... " + str(len(allIssues)))
+        body = getPage(offset)
+        allIssues += body["data"]
+        offset = body["next"]
+        if int(offset) == body["totalCount"]:
+            break
+    return allIssues
+
+
+def addAttachments(allIssues):
+    def sizeOfImage(fileName):
+        basename = os.path.basename(fileName)
+        try:
+            (w, h) = imagesize.get(fileName)
+            if w + h < 0:
+                return None
+            return w, h
+        except Exception as exc:
+            print("Error probing image size: " + basename)
+            return None
+
+    def upload(fileName, issueId):
+        basename = os.path.basename(fileName)
+        sizes = sizeOfImage(fileName)
+        storagePrefix = "file" if sizes is None else "image"
+        # Get upload url
+        resp = requests.post(SPACES_URL + "/api/http/uploads", headers=mkHeaders(),
+                             data=json.dumps({"storagePrefix": storagePrefix}))
+        resp.raise_for_status()
+        uploadPath = resp.content.decode("utf-8").replace('"', "")
+
+        ctype = magic.Magic(mime=True).from_file(fileName)
+        print(basename + " of type " + ctype)
+        headers = mkHeaders(contentType=ctype)
+        resp = requests.put(SPACES_URL + uploadPath + "/" + urllib.parse.quote_plus(basename), headers=headers,
+                            data=open(fileName, 'rb'))
+        resp.raise_for_status()
+
+        attachmentId = resp.content.decode("utf-8")
+
+
+        if "video" in ctype:
+            print("Upload as video file: ", basename)
+            data = {
+                "attachments": [
+                    {"className": "VideoAttachment", "id": attachmentId,
+                     "sizeBytes": os.path.getsize(fileName), "name": basename}
+                ]
+            }
+        elif "image" in ctype:
+            print("Upload as image")
+            (w, h) = sizes
+            data = {
+                "attachments": [
+                    {
+                        "className": "ImageAttachment",
+                        "id": attachmentId,
+                        "name": basename,
+                        "width": w,
+                        "height": h
+                    }
+                ]
+            }
+        else:
+            print("Upload as generic file " + basename)
+            data = {
+                "attachments": [
+                    {"className": "FileAttachment", "id": attachmentId,
+                     "sizeBytes": os.path.getsize(fileName), "filename": basename}
+                ]
+            }
+
+        # Attach upload to issue
+        resp = requests.post(
+            SPACES_URL + f"/api/http/projects/{SPACES_PROJECT_ID}/planning/issues/{issueId}/attachments",
+            data=json.dumps(data),
+            headers=mkHeaders(contentType="application/json")
+        )
+        resp.raise_for_status()
+
+    for _i in allIssues:
+        import glob
+        jiraId = re.search("\[RORDEV-[0-9]{1,3}\]", _i["title"]).group(0).replace("[", "").replace("]", "")
+        filesNames = glob.glob("./attachments/" + jiraId + " */*")
+        if len(filesNames) == 0:
+            continue
+        for f in filesNames:
+            print("======== Uploads for " + jiraId)
+            upload(f, _i["id"])
+            print("============================")
+
+
+def deleteIssue(issueId):
+    print("delete issue: " + issueId)
+    requests.delete(
+        SPACES_URL + "/api/http/projects/id:" + SPACES_PROJECT_ID + "/planning/issues/id:" + issueId,
+        headers=mkHeaders(),
+    )
+
+
 if __name__ == '__main__':
+    # Scan existing issues and delete them
+    allIssues = scanIssuesIdMap()
+    for i in allIssues:
+        deleteIssue(i["id"])
+
+    # Parse JIRA issues from RSS dump file and insert into Space
     with open(JIRA_RSS_EXPORT_FILE) as fp:
         doc = BeautifulSoup(fp, 'html.parser')
     jiraId2usernameMap = scanForJiraUsers(doc)
     issues = parseJiraDump(doc, jiraId2usernameMap)
     for i in issues:
         insert(i)
+
+
+    # Scan again new issues and dump all issues to json file
+    allIssues = scanIssuesIdMap()
+    with open("allIIssues.json", 'w+') as out:
+        out.write(json.dumps(allIssues) + '\n')
+
+    # 3. UPLOAD ATTACHMENTS
+    with open('allIIssues.json') as json_file:
+        allIssues = json.load(json_file)
+        addAttachments(allIssues)
+
+# FILE OK
